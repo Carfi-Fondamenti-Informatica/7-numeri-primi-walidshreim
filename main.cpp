92467354e6d77957a6fa5bfe18297d1be27b3118
walidshreim <91838654+walidshreim@users.noreply.github.com>
2021-12-01 23:28:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,7 @@
 #include<iostream>
+#include "lib.h"
 using namespace std;
-int primecheck(int n,int i)
-{
-    int count=0;
-    if(i==1)
-    {
-        return 1;
-    }
-    else
-    {
-        if(n%i==0)
-        {
-            count=1;
-        }
-        else
-        {
-            return primecheck(n,i-1);
-        }
-    }
-}
+
 int main() {
     int n;
     int check;
