055ea7d64eaf2d9f320c66531270f4bf35d57ba8
walidshreim <91838654+walidshreim@users.noreply.github.com>
2021-12-01 23:07:21 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
-
+int main() {
+    int n;
+    int check;
+    cout << ("ENTER A NUMBER : ");
+    cin >> n;
+    check = primecheck(n, n / 2);
+    if (check == 0) {
+        cout << ("numero non primo\n");
+    } else {
+        cout << ("numero primo\n");
+    }
+}
