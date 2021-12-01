4b15906517df6af72088d660f66807dc94f47add
walidshreim <91838654+walidshreim@users.noreply.github.com>
2021-12-01 23:03:16 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,19 @@
-
+int primecheck(int n,int i)
+{
+    int count=0;
+    if(i==1)
+    {
+        return 1;
+    }
+    else
+    {
+        if(n%i==0)
+        {
+            count=1;
+        }
+        else
+        {
+            return primecheck(n,i-1);
+        }
+    }
+}
