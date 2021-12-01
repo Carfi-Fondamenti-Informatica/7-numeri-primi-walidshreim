1bbe7464077fd6a5c809293646d309c7ef32619f
walidshreim <91838654+walidshreim@users.noreply.github.com>
2021-12-01 23:25:00 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,8 +1,20 @@
-    cout << ("ENTER A NUMBER : ");
-    cin >> n;
-    check = primecheck(n, n / 2);
-    if (check == 0) {
-        cout << ("numero non primo\n");
-    } else {
-        cout << ("numero primo\n");
+#include "lib.h"
+int primecheck(int n,int i)
+{
+    int count=0;
+    if(i==1)
+    {
+        return 1;
     }
+    else
+    {
+        if(n%i==0)
+        {
+            count=1;
+        }
+        else
+        {
+            return primecheck(n,i-1);
+        }
+    }
+}
